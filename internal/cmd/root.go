2250438271f6353c37c83209e192edda2c88900c
revett <2796074+revett@users.noreply.github.com>
2022-05-18 19:31:09 +0100

    Validate argument using cobra

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -3,15 +3,18 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/revett/sepias/internal/note"
 	"github.com/spf13/cobra"
 )
 
 // Root returns a cobra.Command type that acts as the entrypoint CLI command.
 func Root() *cobra.Command {
 	return &cobra.Command{
-		Use:   "sepias",
-		Short: "Tool that @revett uses to manage his notes",
-		RunE:  runE,
+		Use:       "sepias",
+		Short:     "Tool that @revett uses to manage his notes",
+		Args:      cobra.ExactValidArgs(1),
+		ValidArgs: note.Types(),
+		RunE:      runE,
 	}
 }
 
