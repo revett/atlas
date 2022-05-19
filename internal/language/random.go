5d360ecc2007cebfcf6e814105e098c4128d4900
Charlie Revett <2796074+revett@users.noreply.github.com>
2022-05-19 08:17:35 +0100

    Refactor Dictionary (#6)

--- internal/language/random.go
+++ internal/language/random.go
@@ -1,59 +1,45 @@
 package language
 
 import (
-	"errors"
-	"fmt"
-	"io"
 	"math/rand"
-	"os"
 	"strings"
 )
 
 const stringChunkLength = 8
 
+type generatorFunc func(int) (string, error)
+
 // RandomPhrase generates a random identifier made up of english words of a
 // given length.
 //
-// e.g. "unpaste-scripturalist-toxicology-presidentship"
+// e.g. "unpaste-scripturalist-toxicology-presidentship".
 //
 // It makes use of "/usr/share/dict/words" which is an UNIX specific word list,
 // thus if this does not exist then it will generate random letter combinations
 // instead.
 //
 // e.g. "ohylwsib-ttgfjwlj-hwqbxlyj-owyrlces".
-// TODO: this should not have to be concerned about the dictionary, refactor.
-func RandomPhrase(size int) (string, error) {
-	gf := generatePhrase
-	if _, err := os.Stat("/usr/share/dict/words"); errors.Is(err, os.ErrNotExist) {
+func RandomPhrase(dictionary []string, size int) (string, error) {
+	gf := generatePhrase(dictionary)
+	if dictionary != nil {
 		gf = generateString
 	}
 
 	return gf(size)
 }
 
-func generatePhrase(size int) (string, error) {
-	f, err := os.Open("/usr/share/dict/words")
-	if err != nil {
-		return "", fmt.Errorf("failed to open '/usr/share/dict/words': %w", err)
+func generatePhrase(dictionary []string) generatorFunc {
+	return func(size int) (string, error) {
+		var phrase []string
+
+		for i := 0; i < size; i++ {
+			phrase = append(phrase, dictionary[rand.Int()%len(dictionary)])
+		}
+
+		return strings.ToLower(
+			strings.Join(phrase, "-"),
+		), nil
 	}
-
-	bytes, err := io.ReadAll(f)
-	if err != nil {
-		return "", fmt.Errorf(
-			"failed to read contents of '/usr/share/dict/words': %w", err,
-		)
-	}
-
-	t := strings.Split(string(bytes), "\n")
-	var phrase []string
-
-	for i := 0; i < size; i++ {
-		phrase = append(phrase, t[rand.Int()%len(t)])
-	}
-
-	return strings.ToLower(
-		strings.Join(phrase, "-"),
-	), nil
 }
 
 func generateString(size int) (string, error) {
