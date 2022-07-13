ccc6e03b03be82a0ee429ded17a87d771514bf17
revett <2796074+revett@users.noreply.github.com>
2022-07-13 18:17:58 +0100

    Add new `sepias doctor` command which validates all *.md files in the local knowledge base.

---
+++ internal/base/read.go
@@ -0,0 +1,33 @@
+package base
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// Read reads the current directory, and returns a slice of all fs.DirEntry
+// elements which are a file and have the filenmae extension of ".md".
+func Read(path string) ([]fs.DirEntry, error) {
+	dirEntries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read directory at '%s': %w", path, err)
+	}
+
+	notes := []fs.DirEntry{}
+
+	for _, dirEntry := range dirEntries {
+		if !dirEntry.Type().IsRegular() {
+			continue
+		}
+
+		if filepath.Ext(dirEntry.Name()) != ".md" {
+			continue
+		}
+
+		notes = append(notes, dirEntry)
+	}
+
+	return notes, nil
+}
---
+++ internal/cmd/doctor.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/revett/sepias/internal/base"
+	"github.com/revett/sepias/internal/input"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+// Doctor returns a cobra.Command type that checks that each note within the
+// knowledge base is valid, using a number of different validation checks.
+func Doctor() *cobra.Command {
+	return &cobra.Command{
+		Use:                   "doctor",
+		Short:                 "Validates existing notes",
+		DisableFlagsInUseLine: true,
+		Args:                  cobra.NoArgs,
+		RunE:                  doctorRunE,
+	}
+}
+
+func doctorRunE(c *cobra.Command, args []string) error {
+	path, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("unable to get current working directory: %w", err)
+	}
+
+	log.Info().Str("path", path).Msg("validating knowledge base")
+
+	notes, err := base.Read(path)
+	if err != nil {
+		return fmt.Errorf("failed to read notes in knowledge base: %w", err)
+	}
+
+	log.Info().Int("count", len(notes)).Msgf("found %d notes", len(notes))
+
+	var foundErrors []error
+
+	for _, note := range notes {
+		withoutExtension := strings.TrimSuffix(
+			note.Name(), filepath.Ext(note.Name()),
+		)
+
+		if err := input.ValidateTitle(withoutExtension); err != nil {
+			foundError := fmt.Errorf(
+				"invalid note title found in '%s' file: %w", note.Name(), err,
+			)
+			foundErrors = append(foundErrors, foundError)
+		}
+	}
+
+	log.Info().
+		Int("count", len(foundErrors)).
+		Msgf("found %d validation errors", len(foundErrors))
+
+	for _, foundError := range foundErrors {
+		log.Warn().Msg(foundError.Error())
+	}
+
+	return nil
+}
