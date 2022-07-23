6b5dd68325b7fc2d20d07cb56418d270e44dcaf2
revett <2796074+revett@users.noreply.github.com>
2022-07-23 21:37:42 +0100

    Add new flag to root command called `--auto-doctor`, which is set to true by default, which automatically runs the doctor command before creating the new note to ensure that the knowledge base remains valid.

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -9,20 +9,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var autoDoctor bool
+
 // Root returns a cobra.Command type that acts as the entrypoint CLI command.
 func Root() *cobra.Command {
-	return &cobra.Command{
-		Use:                   "sepias {area|entity|interview|project|review|scratch|system}",
-		Example:               "sepias scratch",
-		DisableFlagsInUseLine: true,
-		Short:                 "Tool that @revett uses to manage his notes",
-		Args:                  cobra.ExactValidArgs(1),
-		ValidArgs:             hierarchy.Schemas(),
-		RunE:                  rootRunE,
+	root := cobra.Command{
+		Use:       "sepias {area|entity|interview|project|review|scratch|system}",
+		Example:   "sepias scratch",
+		Short:     "Tool that @revett uses to manage his notes",
+		Args:      cobra.ExactValidArgs(1),
+		ValidArgs: hierarchy.Schemas(),
+		RunE:      rootRunE,
 	}
+
+	root.Flags().BoolVar(
+		&autoDoctor,
+		"auto-doctor",
+		true,
+		"Run the doctor command before creating a new note",
+	)
+
+	return &root
 }
 
 func rootRunE(c *cobra.Command, args []string) error {
+	if autoDoctor {
+		doctor := Doctor()
+		if err := doctor.RunE(nil, nil); err != nil {
+			return fmt.Errorf("failed to run the doctor command before: %w", err)
+		}
+	}
+
 	n, err := note.NewNote(args[0])
 	if err != nil {
 		return fmt.Errorf("failed to create new note type: %w", err)
