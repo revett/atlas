b4c80a9d9c8766d6ae418ed2eac0cf30f74f2025
revett <2796074+revett@users.noreply.github.com>
2023-03-09 21:41:32 +0000

    Remove rand.Seed as no longer required in Go 1.20

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	_ "embed"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/revett/atlas/internal/cmd"
 	"github.com/revett/atlas/internal/cmdv2"
@@ -19,8 +17,6 @@ import (
 var version string
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	log.Logger = log.Output(
 		zerolog.ConsoleWriter{
 			Out: os.Stdout,
