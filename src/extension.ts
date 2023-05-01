127a455af88a39f6dd0e922800d49797f2ccc123
revett <2796074+revett@users.noreply.github.com>
2023-05-01 21:26:08 +0100

    Remove old commands.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,6 @@
 import * as dayjs from "dayjs";
 import * as vscode from "vscode";
 import { openDocumentation } from "./commands/openDocumentation";
-import { createScratchNote } from "./commands/createScratchNote";
-import { createTopicNote } from "./commands/createTopicNote";
 import { createNote } from "./commands/createNote";
 var advancedFormatPlugin = require("dayjs/plugin/advancedFormat");
 var timezonePlugin = require("dayjs/plugin/timezone");
@@ -11,12 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
   dayjs.extend(advancedFormatPlugin);
   dayjs.extend(timezonePlugin);
 
-  context.subscriptions.push(
-    createNote(),
-    createScratchNote(),
-    createTopicNote(),
-    openDocumentation()
-  );
+  context.subscriptions.push(createNote(), openDocumentation());
 }
 
 export function deactivate() {}
